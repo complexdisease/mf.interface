44521f59758f6d2a2f9b9c038759640fb92399f2
complexdisease <69746999+complexdisease@users.noreply.github.com>
2023-08-09 23:24:16 -0700

    Create motif.r

---
+++ motif.r
@@ -0,0 +1,37 @@
+library(Signac)
+library(Seurat)
+library(JASPAR2020)
+library(TFBSTools)
+library(BSgenome.Hsapiens.UCSC.hg38)
+library(patchwork)
+
+setwd('/path/to/workdir/')
+seu <- readRDS("/path/to/rds")
+DefaultAssay(seu) <- "ATAC"
+main.chroms <- standardChromosomes(BSgenome.Hsapiens.UCSC.hg38)
+keep.peaks <- as.logical(seqnames(granges(seu)) %in% main.chroms)
+seu <- seu[keep.peaks, ]
+pfm <- getMatrixSet(
+  x = JASPAR2020,
+  opts = list(collection = "CORE", tax_group = 'vertebrates', all_versions = FALSE)
+)
+seu <- AddMotifs(
+  object = seu,
+  genome = BSgenome.Hsapiens.UCSC.hg38,
+  pfm = pfm
+)
+
+##chromvar
+seu <- RunChromVAR(
+  object = seu,
+  genome = BSgenome.Hsapiens.UCSC.hg38
+)
+# Footprinting
+features=c("TP63","GATA3","SPI1", "RUNX2")
+seu <- Footprint(
+  object = seu,
+  motif.name = features,
+  genome = BSgenome.Hsapiens.UCSC.hg38
+)
+
+p2 <- PlotFootprint(seu, group.by='subclass',idents = c("VCT","EVT","SCT","M","dNK","Epi"), features = features)
