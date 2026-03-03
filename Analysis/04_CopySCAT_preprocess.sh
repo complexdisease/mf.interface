d78f06c2d4b77dfd04ed046430e4b2cfebe4a1a0
complexdisease <69746999+complexdisease@users.noreply.github.com>
2026-03-03 14:27:24 -0800

    Update 04_CopySCAT_preprocess.sh

--- Analysis/04_CopySCAT_preprocess.sh
+++ Analysis/04_CopySCAT_preprocess.sh
@@ -1,7 +1,8 @@
 DIR=$1
-cd $DIR
-ls -d |while read ID;
+cd ${DIR}
+for ID in */;
 do
         mkdir -p ${DIR}/${ID}/copyscat
         python3 ~/tools/CopyscAT/process_fragment_file.py -i ${DIR}/${ID}/outs/atac_fragments.tsv.gz -o ${DIR}/${ID}/copyscat/processed_output.tsv -g /genome/copyscat_reference/hg38_chrom_sizes.tsv -b 1000000 -f 10000
-done
+done &
+wait
