36dce2246a683ef863e47cf7560d2c7d77812b4f
complexdisease <69746999+complexdisease@users.noreply.github.com>
2026-03-03 14:40:17 -0800

    Update 05_souporcell.sh

--- Analysis/05_souporcell.sh
+++ Analysis/05_souporcell.sh
@@ -1,6 +1,6 @@
 DIR="/path/to/cellranger_output/"
 ID=$1
-OUTPUT=2
+OUTPUT=$2
 cd ${OUTPUT}
 mkdir -p fq bam vcf cts
 python renamer.py --bam ${DIR}/${ID}/outs/gex_possorted_bam.bam --barcodes ${DIR}/${ID}/outs/barcodes.tsv --out fq/${ID}_rename.fq
@@ -11,7 +11,7 @@ python retag.py --sam bam/${ID}_minimap.sam --out bam/${ID}_minitagged.bam
 samtools sort -@ 10 bam/${ID}_minitagged.bam bam/${ID}_minitagged_sorted.bam
 samtools index bam/${ID}_minitagged_sorted.bam
 rm bam/${ID}_minimap.sam bam/${ID}_minitagged.bam
-freebayes -f /path/to/reference/GRChg38.fq -iXu -C 2 -q 20 -n 3 -E 1 -m 30 --min-coverage 6 --limit-coverage 100000 bam/${ID}_minitagged.bam > vcf/${ID}.vcf
-vartrix --umi --mapq 30 -b bam/${ID}_minitagged.bam -c ${DIR}/${ID}/outs/barcodes.tsv --scoring-method coverage --threads 16 --ref-matrix cts/${ID}_ref.mtx \
---out-matrix cts/${ID}_alt.mtx -v vcf/${ID}.vcf --fasta fq/${ID}_rename.fq
+freebayes -f /path/to/reference/GRChg38.fa -iXu -C 2 -q 20 -n 3 -E 1 -m 30 --min-coverage 6 --limit-coverage 100000 bam/${ID}_minitagged_sorted.bam > vcf/${ID}.vcf
+vartrix --umi --mapq 30 -b bam/${ID}_minitagged_sorted.bam -c ${DIR}/${ID}/outs/barcodes.tsv --scoring-method coverage --threads 16 --ref-matrix cts/${ID}_ref.mtx \
+--out-matrix cts/${ID}_alt.mtx -v vcf/${ID}.vcf --fasta /path/to/reference/GRChg38.fa
 souporcell -a cts/${ID}_alt.mtx -r cts/${ID}_ref.mtx -b ${DIR}/${ID}/outs/barcodes.tsv -k 2 -t 8 > ${ID}_clusters.tsv
